d1fbb92a8e1e93acea7f0b04c1b1b28abc9d2641
olenaZaruba <122553165+olenaZaruba@users.noreply.github.com>
2023-09-07 17:06:53 +0000

    Deploying to gh-pages from @ olenaZaruba/goit-js-hw-09@246a4447ee1a8bfe5a5e9fdcdd7bfc41c8cb7faf 🚀

---
+++ 03-promises.6333b4d2.js
@@ -0,0 +1,2 @@
+document.querySelector(".form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.elements,r=t.delay;t.step,t.amount,console.log(r.value),e.currentTarget.reset()}));
+//# sourceMappingURL=03-promises.6333b4d2.js.map
---
+++ 03-promises.82641b1b.js
@@ -0,0 +1,2 @@
+({form:document.querySelector(".form")}).form.addEventListener("submit",(function(e){e.preventDefault();const{delay:t,step:r,amount:n}=e.currentTarget.elements;console.log(t.value),e.currentTarget.reset()}));
+//# sourceMappingURL=03-promises.82641b1b.js.map
