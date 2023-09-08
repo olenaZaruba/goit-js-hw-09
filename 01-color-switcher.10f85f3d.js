1eed72aeea29054e0ace96cc1de39512d3bfc6e5
olenaZaruba <122553165+olenaZaruba@users.noreply.github.com>
2023-09-08 18:22:34 +0000

    Deploying to gh-pages from @ olenaZaruba/goit-js-hw-09@92b73c711c267967b74d0f96efc177a8bba3247e 🚀

--- 01-color-switcher.10f85f3d.js
+++
@@ -1,2 +0,0 @@
-const t={startBtn:document.querySelector("[data-start]"),stopBtn:document.querySelector("[data-stop]"),body:document.querySelector("body")};t.stopBtn.disabled=!0;let e=null;t.startBtn.addEventListener("click",(()=>{e=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;t.body.style.backgroundColor=e}),1e3),t.startBtn.disabled=!0,t.stopBtn.disabled=!1})),t.stopBtn.addEventListener("click",(()=>{clearInterval(e),t.startBtn.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.10f85f3d.js.map
